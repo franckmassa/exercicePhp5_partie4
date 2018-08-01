ed3fdc8464c67cfad61043535023fa99bc2dcced
massa <massafranck@gmail.com>
2018-08-01 21:52:08 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
+<?php
+// Fonction qui renvoie un nombre et une chaine de caracteres
+  function concatNumberString($number, $string){
+    return $number . ' ' . $string;
+  };
+?>
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice5</title>
-</head>
-<body>
-  <p>
-    <?php
-    //Déclaration de la fonction concaténation d'un nombre et d'une chaine de caractères
-    //et retourne la contaténation
-    function conca($number, $chain){
-      return $number. '<br/>'. $chain;
-    }
-    //Affichage du nombre et de la chaine de caractères
-    echo conca(5, 'Bonjour');
-    ?>
-  </p>
-</body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p><?= concatNumberString(100, 'string'); ?></p>
+  </body>
 </html>
